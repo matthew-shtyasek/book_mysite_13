3e52df81261a9b2c1fd13e2c8ca0751c90eb9110
Matthew Giovannini <shtyasek2003@mail.ru>
2022-08-19 17:13:45 +0500

    + admin

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from courses.models import Subject, Course, Module
+
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug']
+    prepopulated_fields = {'slug': ('title', )}
+
+
+class ModuleInline(admin.StackedInline):
+    model = Module
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'subject', 'created']
+    list_filter = ['created', 'subject']
+    search_fields = ['title', 'slug', 'overview']
+    prepopulated_fields = {'slug': ('title', )}
+    inlines = [ModuleInline]
