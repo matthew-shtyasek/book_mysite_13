7e6e5483115ec1c830671ad16fa3a4b40de45b96
Matthew Giovannini <shtyasek2003@mail.ru>
2022-08-24 08:22:14 +0500

    + ViewSet + Router (for viewset)

--- courses/api/urls.py
+++ courses/api/urls.py
@@ -1,11 +1,16 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
 from courses.api import views
 
 app_name = 'courses'
 
+router = DefaultRouter()
+router.register('courses', views.CourseViewSet)
+
 urlpatterns = [
     path('subjects/', views.SubjectListView.as_view(), name='subject_list'),
     path('subjects/<pk>/', views.SubjectDetailView.as_view(), name='subject_detail'),
     path('courses/<pk>/enroll/', views.CourseEnrollView.as_view(), name='course_enroll'),
+    path('', include(router.urls)),
 ]
