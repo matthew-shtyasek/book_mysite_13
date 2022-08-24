ab8ccc72e6a515f4983e7e8dd5579787db948a4e
Matthew Giovannini <shtyasek2003@mail.ru>
2022-08-24 09:23:25 +0500

    + setting up postgres + check before deploy

--- book_mysite_10/settings/pro.py
+++ book_mysite_10/settings/pro.py
@@ -9,8 +9,15 @@ ADMINS = (
 ALLOWED_HOSTS = ['*']
 
 DATABASES = {
-    'default': {},
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'educa',
+        'USER': 'educa',
+        'PASSWORD': '123456',
+    },
 }
 
 # export DJANGO_SETTINGS_MODULE=book_mysite_10.settings.pro - env var for current console session
 # python manage.py migrate --settings=book_mysite_10.settings.pro - for current program
+
+#python manage.py check --deploy - check project for deploy (this command can run without deploy flag)
