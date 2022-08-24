05faf84eb02536095497f04b57549688e400d7cc
Matthew Giovannini <shtyasek2003@mail.ru>
2022-08-24 08:32:39 +0500

    + custom action to ViewSet

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,9 +1,8 @@
 from rest_framework import generics, viewsets
 from rest_framework.authentication import BasicAuthentication
-from rest_framework.generics import get_object_or_404
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from courses.api.serializers import SubjectSerializer, CourseSerializer
 from courses.models import Subject, Course
@@ -19,16 +18,12 @@ class SubjectDetailView(generics.RetrieveAPIView):
     serializer_class = SubjectSerializer
 
 
-class CourseEnrollView(APIView):
-    authentication_classes = (BasicAuthentication, )
-    permission_classes = (IsAuthenticated, )
-
-    def post(self, request, pk, format=None):
-        course = get_object_or_404(Course, pk=pk)
-        course.students.add(request.user)
-        return Response({'enrolled': True})
-
-
 class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+    @action(detail=True, methods=['post'], authentication_classes=[BasicAuthentication], permission_classes=[IsAuthenticated])
+    def enroll(self, request, *args, **kwargs):
+        course = self.get_object()
+        course.students.add(request.user)
+        return Response({'enrolled': True})
